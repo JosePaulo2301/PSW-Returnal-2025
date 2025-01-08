f042d659d5c41052d3f2298daca78707e5706d4b
JoseDev2301 <jose.dev2301@gmail.com>
2025-01-08 09:32:36 -0300

    changes views.py and database.py

--- Project-1/management-system-signature/views/view.py
+++ Project-1/management-system-signature/views/view.py
@@ -1,7 +1,7 @@
 # Lógica da aplicação
 import __init__
 from models.database import engine
-from models.model import Subscription
+from models.model import Payment, Subscription
 from sqlmodel import Session, select
 from datetime import date
 
@@ -20,9 +20,19 @@ class SubscribeService:
             statement = select(Subscription)
             result = session.exec(statement).all()
             return result
+            
+    def pay(self, subscription: Subscription):
+        with Session(self.engine) as session:
+            statement = select(Payment).where(Subscription.empresa == subscription.empresa)
+            result = session.exec(statement).all()
+
+            pago = False
+            for res in result:
+                print(res.date)
+
 
 ss = SubscribeService(engine)
 #subscription = Subscription(empresa='Youtube', site='youtube.com.br', data_assinatura=date.today(), valor=49.90)
 #ss.create(subscription)
-
-print(ss.list_all())
+#print(ss.list_all())
+ss.pay(Subscription)
